19b75400450058f79d5e6e5927ebbbee582b1a83
Josh Murtha <murthajosh@gmail.com>
2022-03-02 19:35:19 -0600

    initial commit

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
 let random1 = Math.floor((Math.random() * startupX.length));
 let random2 = Math.floor((Math.random() * startupY.length));
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+console.log('A startup that is like ' + startupX[random1] + ', but for ' + startupY[random2]);
