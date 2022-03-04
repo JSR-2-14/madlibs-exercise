6652ae1628982d22baf70a50dda5d889b161a173
Eric Limjoco <ericlimjoco@gmail.com>
2022-03-04 16:08:05 -0500

    Add show/hide favorites

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ let random2 = () => Math.floor((Math.random() * startupY.length));
 
 console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
 
-const createStartup = () => `A startup that is ${startupX[random1()]} but for ${startupY[random2()]}`
 let startUp = ''
 const favorites = []
 
 const createButton = document.getElementById('create')
 const favoriteButton = document.getElementById('save')
 const printButton = document.getElementById('print')
+const favoritesList = document.getElementById('favorites')
+
+const updateFavorites = () => {
+  let result = ''
+  favorites.forEach(favorite => result+=`${favorite}\n`)
+  favoritesList.innerText = result
+}
 
 createButton.addEventListener('click', () => {
-  startUp = createStartup()
+  startUp = `A startup that is ${startupX[random1()]} but for ${startupY[random2()]}`
   document.getElementById('xForY').innerText = startUp
   favoriteButton.removeAttribute('disabled')
   favoriteButton.innerText = 'Favorite Startup'
@@ -25,12 +31,18 @@ favoriteButton.addEventListener('click', () => {
   favorites.push(startUp)
   favoriteButton.setAttribute('disabled', true)
   favoriteButton.innerText = 'Favorited'
+  updateFavorites()
 })
 
 printButton.addEventListener('click', () => {
-  let result = ''
-  favorites.forEach(favorite => result+=`${favorite}\n`)
-  document.getElementById('favorites').innerText = result
+  if (favoritesList.style.display == 'none') {
+    favoritesList.style.display = 'block'
+    printButton.innerText = 'Hide Favorites'
+  } else {
+    favoritesList.style.display = 'none'
+    printButton.innerText = 'Show Favorites'
+  }
 })
 
 favoriteButton.setAttribute('disabled', true)
+favoritesList.style.display = 'none'
