09e3417f849d68b2ef54d307eb7249a852a2b2bb
Michael Thompson <mikethompson100@hotmail.com>
2022-03-04 21:43:14 -0500

    Homework 1 completed

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
+const favStartUpX = [];
+const favStartUpY = [];
+let random1;
+let random2;
 
-let random1 = Math.floor((Math.random() * startupX.length));
-let random2 = Math.floor((Math.random() * startupY.length));
+function newRandomNum(length) {
+return Math.floor((Math.random() * length));
+}
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+function outputMadlib(random1,random2) {    
+    document.getElementById('xForY').innerHTML = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2];
+}
+
+function createMadlib() {
+    random1 = newRandomNum(startupX.length);
+    random2 = newRandomNum(startupY.length);
+    outputMadlib(random1, random2);
+}
+
+function favoriteStartup() {
+    favStartUpX.push(startupX[random1]);
+    favStartUpY.push(startupY[random2]);
+}
+
+function printFavs() {
+    let printFavsResult = "";
+    for (let i = 0; i < favStartUpX.length; i++) {
+        printFavsResult += i +') A startup that is ' + favStartUpX[i] + ', but for ' + favStartUpY[i] + '<br/>';
+    }
+    document.getElementById('favorites').innerHTML = printFavsResult;
+}
