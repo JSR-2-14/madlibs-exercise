2381947582764388798ae860399f8e8d82b00527
JimmyBringus <jbenson2008@gmail.com>
2022-03-09 18:06:37 -0500

    homework completion

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
-const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
+const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter', 'Reddit', 'Goofy', 'AshleyMadison.com'];
+const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest', 'Bananas', 'Cheerios', 'Goobers', 'Hyenas'];
 
 let random1 = Math.floor((Math.random() * startupX.length));
 let random2 = Math.floor((Math.random() * startupY.length));
 
 console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+
+const insertMadLib = function(){
+    document.getElementById("xForY").innerHTML = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]
+}
