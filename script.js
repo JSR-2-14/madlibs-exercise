20e9b029ed9024e768ee9671b99e8f391719cfac
Michael Felsch <michael_felsch@hotmail.com>
2022-03-10 10:52:19 +1100

    Add button functionality

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
 const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
 
-let random1 = Math.floor((Math.random() * startupX.length));
-let random2 = Math.floor((Math.random() * startupY.length));
+const random1 = function() {
+    return Math.floor((Math.random() * startupX.length));
+};
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+const random2 = function() {
+     return Math.floor((Math.random() * startupY.length));
+};
+
+const nodeXForY = document.getElementById("xForY");
+const nodeFavourites = document.getElementById("favorites");
+const bttnCreateFav = document.getElementById("create");
+const bttnSaveFav = document.getElementById("save");
+const bttnPrintFavs = document.getElementById("print");
+
+let currentPhrase;
+const favourites = [];
+
+const randomPhrase = function () {
+    let phrase = 'A startup that is ' + startupX[random1()] + ', but for ' + startupY[random2()];
+    return phrase;
+};
+
+bttnCreateFav.addEventListener("click", function() {
+    currentPhrase = randomPhrase();
+    nodeXForY.innerText = currentPhrase;
+});
+
+bttnSaveFav.addEventListener("click", function() {
+    if (currentPhrase) {
+        if (favourites.indexOf(currentPhrase) === -1) {
+            favourites.push(currentPhrase);
+            console.log("Phrase saved. Total saved phrases: " + favourites.length);
+        } 
+        else {
+            console.log("Phrase already saved");
+        }
+    } 
+    else {
+        console.log("Save Favourite clicked without a phrase being present")
+    }
+});
+
+bttnPrintFavs.addEventListener("click", function() {
+     nodeFavourites.innerHTML = null;
+     favourites.forEach(fav => {
+         let paragraphNode = document.createElement("p");
+         paragraphNode.innerText = fav;
+         nodeFavourites.appendChild(paragraphNode);
+     });
+});
