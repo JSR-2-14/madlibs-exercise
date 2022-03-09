3959060cea0f94b3b07f22df3124e3cd07256ed7
Joelle Barr <joellebarr22@gmail.com>
2022-03-09 18:21:21 -0500

    complete the homework

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
 
-let random1 = Math.floor((Math.random() * startupX.length));
-let random2 = Math.floor((Math.random() * startupY.length));
+const xForY = document.getElementById("xForY")
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+const createButton = document.getElementById("create");
+
+createButton.addEventListener("click", function() {
+
+  let random1 = Math.floor((Math.random() * startupX.length));
+  let random2 = Math.floor((Math.random() * startupY.length));
+
+  xForY.innerText = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2];
+
+})
+
+const favoritesArray = []
+
+const saveButton = document.getElementById("save");
+
+saveButton.addEventListener("click", function() {
+    favoritesArray.push(xForY.innerText)
+})
+
+const favorites = document.getElementById("favorites")
+
+const printButton = document.getElementById("print");
+
+printButton.addEventListener("click", function() {
+    for (let i = 0; i < favoritesArray.length; i++) {
+        const paragraph = document.createElement('p')
+        paragraph.innerText = favoritesArray[i]
+        document.querySelector('body').append(paragraph)
+    }
+})
