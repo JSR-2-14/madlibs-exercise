3b3d4bbd621b95194b4ed12d26b95dd5479199f4
Kevin Quaranta Jr <kquarantajr@gmail.com>
2022-03-09 15:21:23 -0800

    homework update

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
 
-let random1 = Math.floor((Math.random() * startupX.length));
-let random2 = Math.floor((Math.random() * startupY.length));
+// let random1 = Math.floor((Math.random() * startupX.length));
+// let random2 = Math.floor((Math.random() * startupY.length));
+//console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+let createButton = document.getElementById('create');
+let header = document.getElementById('xForY');
+
+createButton.addEventListener('click', () => {
+        let random1 = Math.floor((Math.random() * startupX.length));
+        let random2 = Math.floor((Math.random() * startupY.length));
+        //console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+        header.innerHTML = `A startup that is ${startupX[random1]}, but for ${startupY[random2]}`;
+    }
+)
