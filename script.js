0b9d8ea3eb630f739b8a69e96fed44332920ec5e
Josh Murtha <murthajosh@gmail.com>
2022-03-09 14:24:01 -0600

    got a basic version of the saving and printing working. Should try to list them out in a ul instead of just inline like that.

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
 
 const createButton = document.getElementById('create');
+const saveButton = document.getElementById('save');
+const printButton = document.getElementById('print');
 const displayText = document.getElementById('xForY');
+const favorites = [];
+const displayFavorites = document.getElementById('favorites');
 
 const createPhrase = () => {
     let random1 = Math.floor((Math.random() * startupX.length));
@@ -10,8 +14,27 @@ const createPhrase = () => {
     displayText.innerHTML = `A startup that is like ${startupX[random1]}, but for ${startupY[random2]}`;
 };
 
+const saveFavorite = () => {
+    favorites.push(`${displayText.innerText}`);
+    console.log(favorites); 
+}
+
+const printFavorites = () => {
+    displayFavorites.append(favorites);
+}
+
 createButton.onclick = () => {
     createPhrase();
+    return;
+}
+
+saveButton.onclick = () => {
+    saveFavorite();
+    return;
+}
+
+printButton.onclick = () => {
+    printFavorites();
     return;
 }
 
