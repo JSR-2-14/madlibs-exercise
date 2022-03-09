e43171c156b016f698ce5929aeb7c87a6f3d6e05
Sushil Mayer <skmayer@gmail.com>
2022-03-09 15:21:05 -0800

    updated index & script for homework 1

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
+const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter', "Meta"];
 const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
+const favorites = []
+let madLib;
+let madLibNode;
 
-let random1 = Math.floor((Math.random() * startupX.length));
-let random2 = Math.floor((Math.random() * startupY.length));
+function clickCreate () {
+    document.getElementById('xForY').innerText = ""
+    let random1 = Math.floor((Math.random() * startupX.length));
+    let random2 = Math.floor((Math.random() * startupY.length));
+    madLib = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2];
+    madLibNode = document.createTextNode(madLib);
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+    document.getElementById('xForY').appendChild(madLibNode)
+}
+
+function clickFav () {
+    favorites.push(madLib)
+}
+
+function clickPrint () {
+    document.getElementById('favorites').innerText = favorites.toString();
+    favorites.forEach(fav => {
+        let h2Node = document.getElementById("favorites")
+        let textNode = document.createTextNode(fav) 
+        h2Node.appendChild(textNode)
+    })
+   
+}
