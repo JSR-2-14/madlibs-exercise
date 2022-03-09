3ee82f5f7adb4a12d5c04a3b7f352520aa94883f
aandersonozhoh <89994451+aandersonozhoh@users.noreply.github.com>
2022-03-09 17:21:08 -0600

    finished homework 1

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
+const favoriteLibs = [];
+const xForY = document.getElementById("xForY")
 
-let random1 = Math.floor((Math.random() * startupX.length));
-let random2 = Math.floor((Math.random() * startupY.length));
+document.getElementById("create").onclick = () => {createNew()
+   }
+function createNew() {
+  let random1 = Math.floor((Math.random() * startupX.length));
+  let random2 = Math.floor((Math.random() * startupY.length));
+  const newStartup = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]; 
+  xForY.innerHTML = newStartup
+  }
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+document.getElementById("save").onclick = () => { saveLib()
+  }
+
+function saveLib() {
+  favoriteLibs.push(xForY.innerText)
+  }
+
+document.getElementById("print").onclick = () => { printLib()
+ }
+
+function printLib() {
+   for (let i = 0; i < favoriteLibs.length; i++){
+     const paragraph = document.createElement('p') 
+     paragraph.innerText = favoriteLibs[i]
+     document.querySelector('body').append(paragraph)
+   }
+}
+
