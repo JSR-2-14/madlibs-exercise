e9821002fda8d382d86a88844dbf58e4f4e1d7c9
Andres Alfonso <afar25@gmail.com>
2022-03-09 15:21:04 -0800

    Homework submit

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
-const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
+    const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter', 'Tumblr'];
+    const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest', 'Basecamp'];
 
-let random1 = Math.floor((Math.random() * startupX.length));
-let random2 = Math.floor((Math.random() * startupY.length));
+    let random1 = Math.floor((Math.random() * startupX.length));
+    let random2 = Math.floor((Math.random() * startupY.length));
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+    console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+
+const createStartup = function(){
+    let startupText = ('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+    document.getElementById('xForY').innerHTML = startupText;
+}
+
+function saveFavoriteStartup() {
+    let favoriteStartup = document.getElementById('xForY').innerHTML;
+    const startupArray = [];
+    startupArray.push(favoriteStartup);
+
+    console.log(favoriteStartup);
+}
+saveFavoriteStartup();
+function printFavorites() {
+    let favoritesSaved = startupArray;
+    document.getElementById('favorites').innerHTML = favoritesSaved;
+}
