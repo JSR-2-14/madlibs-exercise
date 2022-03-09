457fe5643beb74dd15233e83adae27e2e05d137a
Banel <banelbucknor@gmail.com>
2022-03-09 18:28:27 -0500

    homework update

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 const startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
 
+const startup = function(){
+
     let random1 = Math.floor((Math.random() * startupX.length));
     let random2 = Math.floor((Math.random() * startupY.length));
 
+        document.getElementById("xForY").innerHTML = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]
+        
+        const xForY = document.getElementById("xForY")
+    }
 
     console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2])
 
-    const insertMadLib = function(){
-
-        document.getElementById("xForY").innerHTML = 'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]
-    }
 
 
-
-// const xForY = document.getElementById("xForY")
 
 // const creatorButton = document.getElementById('create');
 
 // creatorButton.addEventListener("click", function() {
 // })
 
-   /////////////////// 
-    // document.getElementById("create").onclick = () => {createNew()
-    // }
 
     // function addtext(what){
     //     if (document.createTextNode){
