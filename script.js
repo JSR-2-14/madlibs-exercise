dfb3cdaf1731f66ee62e4634f1593fdc9428b9d4
nnorhairy <nnorhairy@gmail.com>
2022-03-10 08:36:10 +0800

    homework finish

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ const startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
 let random1 = Math.floor((Math.random() * startupX.length));
 let random2 = Math.floor((Math.random() * startupY.length));
 
-console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+//console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+
+
+function create() {
+    console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+    return
+}
+
+const createBtn = document.getElementById("create");
+//const createBtn = document.querySelector("create");
+//const display = document.getElementById("xForY");
+
+createBtn.addEventListener("click", function () {
+    document.getElementById('xForY')
+    .innerHTML='A startup that is ' + startupX[random1] + ', but for ' + startupY[random2];
+})
+
+
+
+//const favorites = new Array();
+
+
+
+
+
+
+
+
+
